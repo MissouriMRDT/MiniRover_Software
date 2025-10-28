9da4326676acb6cde86e896f6a8f0b7afcfa0d8c
Brendan Westley <brendan@westley.us>
2025-10-28 18:34:32 -0500

    OLED testing

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "net.h"
+#include "oled.h"
 #include "web.h"
 #include <esp_log.h>
 #include <nvs_flash.h>
@@ -18,4 +19,5 @@ void app_main(void) {
   ESP_LOGI(TAG_MAIN, "ESP_WIFI_MODE_AP");
   wifi_init_softap();
   start_webserver();
+  oled_main();
 }
